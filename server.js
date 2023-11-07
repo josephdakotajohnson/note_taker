5553e1def954cc0731f13c53edf303988d464ff9
josephdakotajohnson <josephdakotajohnson@gmail.com>
2023-11-07 12:32:04 -0600

    server.js completed

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(routes);
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log(`App listening on Port:  ${PORT}  :D`);
+});
+
+console.log("Server started!");
